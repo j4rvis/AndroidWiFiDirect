98adad29edb37e84d6855a9b9952e2d1b14ad374
Michael Schwarz <schwarzm90@gmail.com>
2015-12-22 17:17:56 +0100

    Revert "Wifi as Local Service."
    This reverts commit cb02ecc2b0822fb328304197085fdc0202d0bc41.

--- app/src/main/java/de/j4rvis/wifidirect/ServiceAdapter.java
+++ app/src/main/java/de/j4rvis/wifidirect/ServiceAdapter.java
@@ -1,12 +1,9 @@
 package de.j4rvis.wifidirect;
 
 import android.content.Context;
-import android.net.wifi.p2p.WifiP2pDevice;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.TextView;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -39,27 +36,8 @@ public class ServiceAdapter extends BaseAdapter {
         return 0;
     }
 
-    public void add(ServiceDevice device){
-        if(!mServiceList.contains(device)){
-            mServiceList.add(device);
-            notifyDataSetChanged();
-        }
-    }
-
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        ServiceDevice device = getItem(position);
-        // Check if an existing view is being reused, otherwise inflate the view
-        if (convertView == null)
-            convertView = LayoutInflater.from(mContext)
-                    .inflate(R.layout.list_item_servicedevice, parent, false);
-
-        TextView address = (TextView) convertView.findViewById(R.id.address);
-        TextView service = (TextView) convertView.findViewById(R.id.service);
-        TextView buddy = (TextView) convertView.findViewById(R.id.buddy);
-        address.setText(device.getDevice().deviceAddress);
-        service.setText(device.getRegistrationType());
-        buddy.setText(device.getDevice().deviceName);
-        return convertView;
+        return null;
     }
 }
